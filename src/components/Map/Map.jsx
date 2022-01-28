219910eadbf5ce480c20f099e3ae654bf080f1f6
DanteThuta <dantethuta97@gmail.com>
2022-01-28 11:06:35 +0630

    Creating Interface for Travel Advisor

---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import GoogleMapReact from "google-map-react";
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+// import LocationOutlinedIcon from "@material-ui/icons/LocationOutlined";
+import Rating from "@material-ui/lab";
+
+function map() {
+  return (
+    <div>
+      <h2>map</h2>
+    </div>
+  );
+}
+
+export default map;
